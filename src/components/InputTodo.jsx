124ae6ea9f9b91e92f023645e56f68d9fa9e962c
misatsuruoka <misa.0429@docomo.ne.jp>
2022-11-14 17:05:49 +0900

    Fix: CSSをJSファイルに記載

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+const style = {
+  backgroundColor: '#c1ffff',
+  width: '400px',
+  height: '30px',
+  padding: '8px',
+  margin: '8px',
+  borderRadius: '8px'
+}
+
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-    <div className="input-area">
+    <div style={style}>
     {/* 入力した際にonChangeイベントが発火→onChangeTodoTextが実行 */}
     <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
     {/* 追加ボタンを押した際にonClickAdd関数が実行 */}
