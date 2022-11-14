733a7bca351c3a3b9d901b9efb8fc707bb165ca3
misatsuruoka <misa.0429@docomo.ne.jp>
2022-11-14 12:25:30 +0900

    Add: タスクの追加機能を実装

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,34 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState('');
   const [incompleteTodos, setIncompleteTodos] = useState(['あああああ', 'いいいいい']);
   const [completeTodos, setCompleteTodos] = useState(['ああああ']);
+
+  // 入力したテキストを取得、変数todoTextに値をセット
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    // 入力欄が空のまま追加ボタンを押した場合に追加されないようにする
+    if(todoText === "") return
+    // todoTextを追加した新しい配列を定義
+    const newTodos = [...incompleteTodos, todoText];
+    // 未完了のTODOに値をセット
+    setIncompleteTodos(newTodos);
+    setTodoText("")
+  }
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        {/* 入力した際にonChangeイベントが発火→onChangeTodoTextが実行 */}
+        <input placeholder="TODOを入力" value={todoText} onChange={onChangeTodoText} />
+        {/* 追加ボタンを押した際にonClickAdd関数が実行 */}
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
+          {/* 未完了のTODOごとにdivを作成 */}
           {incompleteTodos.map((todo) => {
             return (
           <div key={todo} className="list-row">
@@ -27,6 +44,7 @@ export const App = () => {
       <div className="complete-area">
       <p className="title">完了のTODO</p>
         <ul>
+          {/* 完了のTODOごとにdivを作成 */}
           {completeTodos.map((todo) => {
             return (
           <div key={todo} className="list-row">
