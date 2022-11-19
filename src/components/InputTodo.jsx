0b068f8c806610689c9651d5663e94e737e965e1
misatsuruoka <misa.0429@docomo.ne.jp>
2022-11-19 21:27:35 +0900

    Fix: エリアの配色を変更

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import { Button } from "@mui/material"
+import React from "react";
+import { Button } from "@mui/material";
 
 const style = {
-  backgroundColor: '#c1ffff',
-  width: '400px',
-  height: '30px',
-  padding: '8px',
-  margin: '8px',
-  borderRadius: '8px'
-}
+  backgroundColor: "#f1e5ab",
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px",
+};
 
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick, disabled } = props;
   return (
     <div style={style}>
-    {/* 入力した際にonChangeイベントが発火→onChangeTodoTextが実行 */}
-    <input disabled={disabled} placeholder="TODOを入力" value={todoText} onChange={onChange} />
-    {/* 追加ボタンを押した際にonClickAdd関数が実行 */}
-    <Button disabled={disabled} variant="contained" color="warning" onClick={onClick}>追加</Button>
-  </div>
-  )
-}
+      {/* 入力した際にonChangeイベントが発火→onChangeTodoTextが実行 */}
+      <input
+        disabled={disabled}
+        placeholder="TODOを入力"
+        value={todoText}
+        onChange={onChange}
+      />
+      {/* 追加ボタンを押した際にonClickAdd関数が実行 */}
+      <button onClick={onClick}>追加</button>
+    </div>
+  );
+};
