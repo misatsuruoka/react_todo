8ff790e4cee30e9d01f46f001a6c2be2494ebe7b
misatsuruoka <misa.0429@docomo.ne.jp>
2022-11-22 22:22:27 +0900

    Fix: タスク入力欄をセンターに配置

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -4,10 +4,11 @@ import { Button, Container } from "@mui/material";
 const style = {
   backgroundColor: "#f1e5ab",
   width: "500px",
-  height: "30px",
+  height: "50px",
   padding: "8px",
   margin: "auto",
   borderRadius: "8px",
+  position: "relative",
 };
 
 export const InputTodo = (props) => {
@@ -15,14 +16,16 @@ export const InputTodo = (props) => {
   return (
     <div style={style}>
       {/* 入力した際にonChangeイベントが発火→onChangeTodoTextが実行 */}
-      <input
-        disabled={disabled}
-        placeholder="TODOを入力"
-        value={todoText}
-        onChange={onChange}
-      />
-      {/* 追加ボタンを押した際にonClickAdd関数が実行 */}
-      <button onClick={onClick}>追加</button>
+      <div className="input-area">
+        <input
+          disabled={disabled}
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChange}
+        />
+        {/* 追加ボタンを押した際にonClickAdd関数が実行 */}
+        <button onClick={onClick}>追加</button>
+      </div>
     </div>
   );
 };
